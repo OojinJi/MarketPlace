161aaaf89e7add0ac8100b1089b28583b456983b
Oojin Ji <Oojin54@gmail.com>
2022-04-22 11:52:28 -0400

    Connected the UI Layer to the Service layer

--- MarketPlace/Controllers/HomeController.cs
+++ MarketPlace/Controllers/HomeController.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutoMapper;
+using MarketPlace.Models;
+using MarketPlace.ServiceLayer;
+using MarketPlace.ServiceLayer.Models;
 
 namespace MarketPlace.Controllers
 {
     public class HomeController : Controller
     {
+        MapperConfiguration config;
+        IMapper mapper;
+        ProductService productService;
+        UserService userService;
+
+        public HomeController()
+        {
+            productService = new ProductService();
+            config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ProductDTO, ProductInfoVM>();
+                cfg.CreateMap<UserDTO, UserInfoVM>();
+            });
+            mapper = config.CreateMapper();
+        } 
         public ActionResult Index()
         {
             return View();
